790bef44bed35f6cff7fa4bf3e3413044325ac18
AeRiaSaito <florence.navet@laplateforme.io>
2024-11-27 11:38:09 +0100

    job 7

---
+++ main7.py
@@ -0,0 +1,20 @@
+#fonction paramètre langage
+def verif_langague(langage):
+    if langage == "JavaScript":
+        print("Tu es un developpeur Web.")
+    elif langage == "Python":  
+        print("Tu es un developeeur IA.")
+    elif langage == "Java":
+        print("Tu es un développeur logiciel")
+    elif langage == "ReactJs":
+        print("Tu es un développeur mobile.")
+    else:
+        print("Un jour, je serai le meilleur développeur !!!")  
+
+#appel fonction
+verif_langague("JavaScript") 
+verif_langague("Python") 
+verif_langague("Java") 
+verif_langague("ReactJs") 
+verif_langague("C++") 
+            
